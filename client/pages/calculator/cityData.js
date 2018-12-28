f6a5c51936420e7538ec52124b8ee17c8c7db3b0
Arki Zhou <arki@thenetcircle.com>
2018-12-28 18:16:44 +0800

    add more cities

--- client/pages/calculator/cityData.js
+++ client/pages/calculator/cityData.js
@@ -27,5 +27,237 @@ module.exports = [{
         gjj: 7
       }
     }
+  }, {
+    key: 'beijing',
+    chinese: '北京市',
+    data: {
+      base: {
+        gjjBaseMin: 2273,
+        gjjBaseMax: 25401,
+        sbBaseMin: 5080,
+        sbBaseMax: 25401
+      },
+      person: {
+        sb: {
+          yanglao: 8,
+          yiliao: 2,
+          shiye: 0.2
+        },
+        gjj: 12
+      },
+      company: {
+        sb: {
+          yanglao: 19,
+          yiliao: 10,
+          shiye: 0.8,
+          gongshang: 0.4,
+          shengyu: 0.8
+        },
+        gjj: 12
+      }
+    }
+  }, {
+    key: 'hangzhou',
+    chinese: '杭州市',
+    data: {
+      base: {
+        gjjBaseMin: 2010,
+        gjjBaseMax: 24311,
+        sbBaseMin: 3054.95,
+        sbBaseMax: 15274.74
+      },
+      person: {
+        sb: {
+          yanglao: 8,
+          yiliao: 2,
+          shiye: 0.5
+        },
+        gjj: 12
+      },
+      company: {
+        sb: {
+          yanglao: 14,
+          yiliao: 10.5,
+          shiye: 0.5,
+          gongshang: 0.2,
+          shengyu: 1.2
+        },
+        gjj: 12
+      }
+    }
+  }, {
+    key: 'guangzhou',
+    chinese: '广州市',
+    data: {
+      base: {
+        gjjBaseMin: 2100,
+        gjjBaseMax: 24654,
+        sbBaseMin: 2100,
+        sbBaseMax: 24654
+      },
+      person: {
+        sb: {
+          yanglao: 8,
+          yiliao: 2,
+          shiye: 0.2
+        },
+        gjj: 5
+      },
+      company: {
+        sb: {
+          yanglao: 14,
+          yiliao: 7,
+          shiye: 0.64,
+          gongshang: 0.2,
+          shengyu: 0.85
+        },
+        gjj: 5
+      }
+    }
+  }, {
+    key: 'shenzhen',
+    chinese: '深圳市',
+    data: {
+      base: {
+        gjjBaseMin: 2130,
+        gjjBaseMax: 25044,
+        sbBaseMin: 2200,
+        sbBaseMax: 25044
+      },
+      person: {
+        sb: {
+          yanglao: 8,
+          yiliao: 2,
+          shiye: 0.5
+        },
+        gjj: 5
+      },
+      company: {
+        sb: {
+          yanglao: 14,
+          yiliao: 6.2,
+          shiye: 0.9,
+          gongshang: 0.168,
+          shengyu: 0.45
+        },
+        gjj: 5
+      }
+    }
+  }, {
+    key: 'wuhan',
+    chinese: '武汉市',
+    data: {
+      base: {
+        gjjBaseMin: 1750,
+        gjjBaseMax: 29881.25,
+        sbBaseMin: 3399.6,
+        sbBaseMax: 19920.9
+      },
+      person: {
+        sb: {
+          yanglao: 8,
+          yiliao: 2,
+          shiye: 0.3
+        },
+        gjj: 8
+      },
+      company: {
+        sb: {
+          yanglao: 19,
+          yiliao: 8,
+          shiye: 0.7,
+          gongshang: 0.48,
+          shengyu: 0.7
+        },
+        gjj: 8
+      }
+    }
+  }, {
+    key: 'nanjing',
+    chinese: '南京市',
+    data: {
+      base: {
+        gjjBaseMin: 2020,
+        gjjBaseMax: 25300,
+        sbBaseMin: 3030,
+        sbBaseMax: 19935
+      },
+      person: {
+        sb: {
+          yanglao: 8,
+          yiliao: 2,
+          shiye: 0.5
+        },
+        gjj: 8
+      },
+      company: {
+        sb: {
+          yanglao: 19,
+          yiliao: 9,
+          shiye: 0.5,
+          gongshang: 0.2,
+          shengyu: 0.8
+        },
+        gjj: 8
+      }
+    }
+  }, {
+    key: 'chengdu',
+    chinese: '成都市',
+    data: {
+      base: {
+        gjjBaseMin: 1500,
+        gjjBaseMax: 20972,
+        sbBaseMin: 3067,
+        sbBaseMax: 15333
+      },
+      person: {
+        sb: {
+          yanglao: 8,
+          yiliao: 2,
+          shiye: 0.4
+        },
+        gjj: 6
+      },
+      company: {
+        sb: {
+          yanglao: 19,
+          yiliao: 6.5,
+          shiye: 0.6,
+          gongshang: 0.22,
+          shengyu: 0.6
+        },
+        gjj: 6
+      }
+    }
+  }, {
+    key: 'chongqing',
+    chinese: '重庆市',
+    data: {
+      base: {
+        gjjBaseMin: 1500,
+        gjjBaseMax: 18318,
+        sbBaseMin: 3664,
+        sbBaseMax: 18318
+      },
+      person: {
+        sb: {
+          yanglao: 8,
+          yiliao: 2,
+          shiye: 0.5
+        },
+        gjj: 7
+      },
+      company: {
+        sb: {
+          yanglao: 19,
+          yiliao: 8.5,
+          shiye: 0.5,
+          gongshang: 0.6,
+          shengyu: 0.5
+        },
+        gjj: 7
+      }
+    }
   }
 ]
